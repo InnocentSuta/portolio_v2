c3802e3d2b5727c3a34c3cac453d1beb6831714f
Innocent Suta <sutainnocent@gmail.com>
2022-09-28 17:44:50 +0300

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {HiMenuAlt4, HiX} from 'react-icons/hi';
+import React, { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 import { images } from '../../constants';
 
@@ -11,11 +11,11 @@ const Navbar = () => {
 
   return (
     <nav className='app__navbar'>
-      <div class='app__navbar-logo'> 
-      <img  src={images.logo} alt ="logo" />
+      <div class='app__navbar-logo'>
+        <img src={images.logo} alt="logo" />
       </div>
       <ul className='app__navbar-links'>
-        {['Home', 'About', 'Experience','Projects','Skills','Contact'].map((item)=>(
+        {['Home', 'About', 'Experience', 'Projects', 'Skills', 'Contact'].map((item) => (
           <li className='app__flex p-text' key={`link-${item}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
@@ -24,17 +24,19 @@ const Navbar = () => {
       </ul>
 
       <div className='app__navbar-menu'>
-        <HiMenuAlt4 onClick={ () =>setToggle(true)} />
-        { toggle && (
-          <motion.div 
-            whileInView={{x : [300,0]}}
-            transition ={{ duration: 0.85, ease: 'easeOut'}}>
-              <HiX onClick={() =>setToggle(false)}/>
-              {['Home', 'About', 'Experience','Projects','Skills','Contact'].map((item)=>(
-                <li key={`link-${item}`}>
-                  <a href={`#${item}`} onClick={() =>setToggle(false)}>{item}</a>
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}>
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {['Home', 'About', 'Experience', 'Projects', 'Skills', 'Contact'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
                 </li>
               ))}
+            </ul>
           </motion.div>
         )}
       </div>
